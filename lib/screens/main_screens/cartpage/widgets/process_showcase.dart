ff463a808e0c04eea3dcc0ae54ede866cefc0409
Marvyoha <125772477+Marvyoha@users.noreply.github.com>
2024-05-15 20:08:36 +0100

    Before null safety refactoring

--- lib/screens/main_screens/cartpage/widgets/process_showcase.dart
+++ lib/screens/main_screens/cartpage/widgets/process_showcase.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:carbon_icons/carbon_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
